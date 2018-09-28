0e18c88ddac7470b7ff91ea07d8dfea24ea503ec
siewyanhoh <jeffrey_siewyan@hotmail.com>
2018-09-28 11:27:37 +0200

    verbatim added

--- src/makePlots.py
+++ src/makePlots.py
@@ -12,13 +12,17 @@ from variables import command
 #samplesConfig=ScanZp("BBbarDM400",50,200)
 #run(samplesConfig,command)
 
-#for processes in [ "BBbarDM" , "BBbarDM400" , "MonojetDM" , "HadronicDM" , "BBbarDM400Match1jet" , "DiJetsDM" ]:
-#    scanGrid=ScanZp("%s"%processes,50,200) #hs,dm
-#    run(scanGrid,command)
-#for processes in [ "BBbarDM" , "BBbarDM400" , "MonojetDM" , "HadronicDM" , "BBbarDM400Match1jet" , "DiJetsDM" ]:
-#    scanGrid=ScanDM("%s"%processes,50,3000) #hs, zp
-#    run(scanGrid,command)
-    
+#Scaning on Zp mass across various process with FIX hs and FIX dm
 for processes in [ "BBbarDM" , "BBbarDM400" , "MonojetDM" , "HadronicDM" , "BBbarDM400Match1jet" , "DiJetsDM" ]:
-    scanGrid=Scanhs("%s"%processes,100,3000) #dm,zp
+    scanGrid=ScanZp("%s"%processes,90,100) #FIX hs,FIX dm
     run(scanGrid,command)
+
+#Scaning on DM mass across various process with FIX hs and FIX zp
+#for processes in [ "BBbarDM" , "BBbarDM400" , "MonojetDM" , "HadronicDM" , "BBbarDM400Match1jet" , "DiJetsDM" ]:
+#    scanGrid=ScanDM("%s"%processes,50,3000) #FIX hs, FIX zp
+#    run(scanGrid,command)
+
+#Scaning on HS mass across various process with FIX dm and FIX zp
+#for processes in [ "BBbarDM" , "BBbarDM400" , "MonojetDM" , "HadronicDM" , "BBbarDM400Match1jet" , "DiJetsDM" ]:
+#    scanGrid=Scanhs("%s"%processes,100,3000) #FIX dm,FIX zp
+#    run(scanGrid,command)
